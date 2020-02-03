12134d8c0f11c3afdf0395bb2a41993b425e0464
Muhammad Usama <37137366+HMUsama@users.noreply.github.com>
2020-02-04 00:33:21 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import InputBox from './components/InputBox';
+export default class App extends Component {
+
+
+
+  render() {
+    return ( 
+      <View>
+        <InputBox />
+      </View>
+    );
+  }
+}
