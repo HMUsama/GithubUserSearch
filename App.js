53332eae45b2c9ba565ecffe8c6e1bdea825d082
Muhammad Usama <37137366+HMUsama@users.noreply.github.com>
2020-02-05 22:31:40 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -4,15 +4,20 @@ import {
   View
 } from 'react-native';
 import InputBox from './components/InputBox';
+import { Provider } from 'react-redux'
+import store from './src/store/index'
+
 export default class App extends Component {
 
 
 
   render() {
-    return ( 
-      <View>
-        <InputBox />
-      </View>
+    return (
+      <Provider store={store}>
+        <View>
+          <InputBox />
+        </View>
+      </Provider>
     );
   }
 }
